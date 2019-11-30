263a60e66a127ff18188d508b17fd9fcdd1fa3ca
Galiks <31892748+Galiks@users.noreply.github.com>
2019-11-30 04:30:25 +0400

    Добавил простой тест

--- WindowsFormsApp2/Test.Designer.cs
+++ WindowsFormsApp2/Test.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.Ответить = new System.Windows.Forms.Button();
+            this.AnswerButton = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
@@ -38,6 +38,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -48,15 +49,17 @@
             this.richTextBox1.Size = new System.Drawing.Size(260, 27);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
             // 
-            // Ответить
+            // AnswerButton
             // 
-            this.Ответить.Location = new System.Drawing.Point(81, 226);
-            this.Ответить.Name = "Ответить";
-            this.Ответить.Size = new System.Drawing.Size(130, 23);
-            this.Ответить.TabIndex = 1;
-            this.Ответить.Text = "Ответить";
-            this.Ответить.UseVisualStyleBackColor = true;
+            this.AnswerButton.Location = new System.Drawing.Point(142, 226);
+            this.AnswerButton.Name = "AnswerButton";
+            this.AnswerButton.Size = new System.Drawing.Size(130, 23);
+            this.AnswerButton.TabIndex = 1;
+            this.AnswerButton.Text = "Ответить";
+            this.AnswerButton.UseVisualStyleBackColor = true;
+            this.AnswerButton.Click += new System.EventHandler(this.AnswerButton_Click);
             // 
             // radioButton1
             // 
@@ -116,7 +119,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(78, 57);
+            this.label1.Location = new System.Drawing.Point(87, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
@@ -125,7 +128,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(120, 57);
+            this.label2.Location = new System.Drawing.Point(129, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
@@ -134,17 +137,28 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(162, 57);
+            this.label3.Location = new System.Drawing.Point(171, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -153,7 +167,7 @@
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.Ответить);
+            this.Controls.Add(this.AnswerButton);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Test";
             this.Text = "Тест";
@@ -166,7 +180,7 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button Ответить;
+        private System.Windows.Forms.Button AnswerButton;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
@@ -175,5 +189,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- WindowsFormsApp2/Test.cs
+++ WindowsFormsApp2/Test.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
@@ -13,6 +8,9 @@ namespace WindowsFormsApp2
     public partial class Test : Form
     {
         private readonly int maxQuestions;
+        private HashSet<string> words;
+        private Random Random { get; set; }
+        private string answer;
         public Test()
         {
             InitializeComponent();
@@ -20,11 +18,62 @@ namespace WindowsFormsApp2
             label1.Text = "0";
             label2.Text = " / ";
             label3.Text = maxQuestions.ToString();
+            words = new HashSet<string>();
+            Random = new Random();
         }
 
         private void Test_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            words.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string randomWord = WordDictionary.EngRandomWordOfDictionary();
+                while (words.Contains(randomWord))
+                {
+                    randomWord = WordDictionary.EngRandomWordOfDictionary();
+                }
+                var radioButton = this.Controls.Find("radioButton" + (i + 1), true).FirstOrDefault() as RadioButton;
+                radioButton.Text = randomWord;
+                words.Add(randomWord);
+            }
+
+
+            int randomNumber = Random.Next(0, 5);
+
+            answer = words.ToList()[randomNumber];
+
+            richTextBox1.Text = "Выберите перевод слова: " + WordDictionary.Dict[answer];
+
+        }
+
+        private void AnswerButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var radioButton = this.Controls.Find("radioButton" + (i + 1), true).FirstOrDefault() as RadioButton;
+                if (radioButton.Checked)
+                {
+                    if (radioButton.Text == answer)
+                    {
+                        MessageBox.Show("ПРАВИЛЬНО!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("НЕПРАВИЛЬНО!");
+                    }
+                }
+            }
         }
     }
 }
